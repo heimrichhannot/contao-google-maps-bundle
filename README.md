0dd362f7b219e8af9afd3fd1d623b8de1e3082e1
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-03-27 16:31:43 +0200

    added initial state

---
+++ README.md
@@ -0,0 +1,8 @@
+# Contao Google Maps Bundle
+
+This bundle offers functionality concerning the Google Maps 3 API.
+
+## Features
+
+- introduces a simple Contao backend entity to configure your Google Map
+- adds frontend modules and content elements
---
+++ src/HeimrichHannotContaoGoogleMapsBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\GoogleMapsBundle;
+
+use HeimrichHannot\GoogleMapsBundle\DependencyInjection\GoogleMapsExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HeimrichHannotContaoGoogleMapsBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getContainerExtension()
+    {
+        return new GoogleMapsExtension();
+    }
+}
