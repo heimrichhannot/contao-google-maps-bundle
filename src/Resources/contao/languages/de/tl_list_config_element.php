ee519b1a71fc19621edcf1c1adbf90bf5c201a7e
Dennis Patzer <d.patzer@heimrich-hannot.de>
2020-05-06 19:31:06 +0200

    fixed autoload

--- src/Resources/contao/languages/de/tl_list_config_element.php
+++ src/Resources/contao/languages/de/tl_list_config_element.php
@@ -5,6 +5,8 @@ $lang = &$GLOBALS['TL_LANG']['tl_list_config_element'];
 /**
  * Reference
  */
-$lang['reference'] += [
-    \HeimrichHannot\GoogleMapBundle\ConfigElementType\GoogleMapListConfigElementType::TYPE => 'Google Map',
-];
+if (\Contao\System::getContainer()->get('huh.utils.container')->isBundleActive('HeimrichHannot\ListBundle\HeimrichHannotContaoListBundle')) {
+    $lang['reference'] += [
+        \HeimrichHannot\GoogleMapBundle\ConfigElementType\GoogleMapListConfigElementType::TYPE => 'Google Map',
+    ];
+}
