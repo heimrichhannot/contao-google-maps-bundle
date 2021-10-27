5c0b3a005ff6fc75c7546b2797c325ab386cc035
Thomas Körner <t.koerner@heimrich-hannot.de>
2021-10-27 14:59:19 +0200

    enhance list config

--- src/Model/GoogleMapModel.php
+++ src/Model/GoogleMapModel.php
@@ -10,6 +10,11 @@ namespace HeimrichHannot\GoogleMapsBundle\Model;
 
 use Contao\Model;
 
+/**
+ * @property string $positioningMode
+ * @property string $boundMode
+ * @property string $centerMode
+ */
 class GoogleMapModel extends Model
 {
     protected static $strTable = 'tl_google_map';
