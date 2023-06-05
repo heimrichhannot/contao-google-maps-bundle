38b2fca50139bae14c6040467770b91d010eeeb8
Thomas Körner <t.koerner@heimrich-hannot.de>
2023-06-05 15:52:31 +0200

    cleanup

--- src/EventListener/PrivacyCenterListener.php
+++ src/EventListener/PrivacyCenterListener.php
@@ -8,31 +8,23 @@
 
 namespace HeimrichHannot\GoogleMapsBundle\EventListener;
 
-use Contao\CoreBundle\ServiceAnnotation\Hook;
 use HeimrichHannot\GoogleMapsBundle\Event\BeforeRenderApiEvent;
-use HeimrichHannot\PrivacyCenter\Model\TrackingObjectModel;
 use HeimrichHannot\PrivacyCenterBundle\Generator\ProtectedCodeGenerator;
 use HeimrichHannot\PrivacyCenterBundle\Manager\PrivacyCenterManager;
 use HeimrichHannot\PrivacyCenterBundle\Script\ExternalScriptFile;
 use Ivory\GoogleMap\Helper\Event\ApiEvents;
 use Ivory\GoogleMap\Helper\Formatter\Formatter;
-use Ivory\GoogleMap\Helper\Renderer\ApiRenderer;
 use Ivory\GoogleMap\Helper\Renderer\Utility\SourceRenderer;
 use Ivory\GoogleMap\Helper\Subscriber\ApiJavascriptSubscriber;
-use Psr\Container\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
-class PrivacyCenterListener implements ServiceSubscriberInterface, EventSubscriberInterface
+class PrivacyCenterListener implements EventSubscriberInterface
 {
-    private ContainerInterface $container;
-//    private ApiRenderer $apiRenderer;
     private ProtectedCodeGenerator $protectedCodeGenerator;
     private PrivacyCenterManager $privacyCenterManager;
 
-    public function __construct(ContainerInterface $container, ProtectedCodeGenerator $protectedCodeGenerator, PrivacyCenterManager $privacyCenterManager)
+    public function __construct(ProtectedCodeGenerator $protectedCodeGenerator, PrivacyCenterManager $privacyCenterManager)
     {
-        $this->container = $container;
         $this->protectedCodeGenerator = $protectedCodeGenerator;
         $this->privacyCenterManager = $privacyCenterManager;
     }
@@ -99,53 +91,6 @@ class PrivacyCenterListener implements ServiceSubscriberInterface, EventSubscrib
 
         $apiSubscriber->handle($event->getApiEvent(), ApiEvents::JAVASCRIPT, $event->getApiHelper()->getEventDispatcher());
         $event->setCode($event->getApiEvent()->getCode());
-    }
-
-    /**
-     * Adjust the generated map api. Priority -1 ensures it's called after the ReplaceDynamicScriptTagsListener
-     * listener.
-     *
-     * @Hook("replaceDynamicScriptTags", priority=-1)
-     */
-    public function onReplaceDynamicScriptTags(string $buffer): string
-    {
-        return $buffer;
-
-        if (
-            !class_exists(ProtectedCodeGenerator::class) ||
-            !$this->container->has(ProtectedCodeGenerator::class) ||
-            !isset($GLOBALS['TL_BODY']['huhGoogleMaps'])) {
-            return $buffer;
-        }
-
-        if (TrackingObjectModel::findBy(['localStorageAttribute=?'], ['google_maps'])) {
-            $code = $this->container->get(ProtectedCodeGenerator::class)->generateProtectedCode(
-                $GLOBALS['TL_BODY']['huhGoogleMaps'],
-                ['google_maps'],
-            );
-
-            if (!empty($code)) {
-                $GLOBALS['TL_BODY']['huhGoogleMaps'] = $code;
-            }
-
-            // protect the code
-//            $GLOBALS['TL_BODY']['huhGoogleMaps'] = PrivacyCenterManager::getInstance()->addProtectedCode(
-//                $GLOBALS['TL_BODY']['huhGoogleMaps'],
-//                ['google_maps'],
-//                [
-//                    'addPoster' => false,
-//                ]
-//            );
-        }
-
-        return $buffer;
-    }
-
-    public static function getSubscribedServices()
-    {
-        return [
-            '?'.ProtectedCodeGenerator::class,
-        ];
     }
 
     public static function getSubscribedEvents()
