d1ab3ebf8e722019c7b22f53c789b9f9389c751a
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-04-30 11:51:24 +0200

    fix error in contao backend

--- src/Resources/contao/languages/de/tl_reader_config_element.php
+++ src/Resources/contao/languages/de/tl_reader_config_element.php
@@ -6,7 +6,7 @@
  * @license LGPL-3.0-or-later
  */
 
-use HeimrichHannot\GoogleMapBundle\ConfigElementType\GoogleMapConfigElementType;
+use HeimrichHannot\GoogleMapsBundle\ConfigElementType\GoogleMapConfigElementType;
 
 $lang = &$GLOBALS['TL_LANG']['tl_reader_config_element'];
 
